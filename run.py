a01c87fa7d15bb3ca57b27e4ad10ddb6a495a41d
Manh Pham <manhp7658@gmail.com>
2024-10-09 09:05:26 +0700

    update index.html

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 import os
+from notify_run import Notify
 
 os.system("python initialize.py")
 os.system("python jinja_render.py")
 os.system(("mv report.html html-output/"))
+os.system("git add .")
+os.system("git commit -m 'update index.html' ")
+os.system("git push origin master")
+notify = Notify()
+notify.send("Si so", "towjacix.github.io")
