d3b6e6f968626ca318064e2955c73634b23f79c3
Manh Pham <54128221+towjacix@users.noreply.github.com>
2025-04-01 22:33:48 +0700

    update index.html

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("python initialize.py")
+os.system("python face-detection.py")
 os.system("python jinja_render.py")
 os.system("git add .")
 os.system("git commit -m 'update index.html' ")
